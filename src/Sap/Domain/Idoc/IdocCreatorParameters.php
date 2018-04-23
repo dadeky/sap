1f3281e5e6c7c61a1cc89cc40b1dcd0bf5a9bab7
dvorski <dadeky@gmail.com>
2018-04-23 08:48:39 +0200

    msguid added

--- src/Sap/Domain/Idoc/IdocCreatorParameters.php
+++ src/Sap/Domain/Idoc/IdocCreatorParameters.php
@@ -8,17 +8,24 @@ class IdocCreatorParameters
     private $qos;
     private $interfaceNamespace;
     private $queueid;
+    private $uuid;
     
     public function __construct(
-        $path=null, $interfaceName=null, $qos=null, $interfaceNamespace=null, $queueid=null
+        $path=null, $interfaceName=null, $qos=null, $interfaceNamespace=null, $queueid=null, $uuid = null
     ){
         $this->interfaceName = $interfaceName;
         $this->path = $path;
         $this->qos = $qos;
         $this->interfaceNamespace = $interfaceNamespace;
         $this->queueid = $queueid;
+        $this->uuid = $uuid;
     }
     
+    public function getUuid()
+    {
+        return $this->uuid;
+    }
+
     public function getQueueid()
     {
         return $this->queueid;
