8f7f1e4d7e00a142ee0e828c0fb0f694e74b89ac
dvorski <darko.dvorski@boxmark.com>
2019-12-02 15:45:42 +0100

    error handling changed a bit

--- src/Sap/Infrastructure/SapService.php
+++ src/Sap/Infrastructure/SapService.php
@@ -33,35 +33,39 @@ class SapService implements RemoteServiceInterface
 	        throw new SoapException($ex->getMessage());
 	    }
 	    
-	    if (isset($rawResponse->ErrorList->item) && count(get_object_vars($rawResponse->ErrorList->item)) > 0)
-        {
-            $errorMessages = [];
-            foreach ($rawResponse->ErrorList as $errItem)
-            {
-                $Msgty = property_exists($errItem, "Msgty") ? $errItem->Msgty : $errItem->MSGTY;
-                $Message = property_exists($errItem, "Message") ? $errItem->Message : $errItem->MESSAGE;
-                $Msgno = property_exists($errItem, "Msgno") ? $errItem->Msgno : $errItem->MSGNO;
-                $Msgv1 = property_exists($errItem, "Msgv1") ? $errItem->Msgv1 : $errItem->MSGV1;
-                $Msgv2 = property_exists($errItem, "Msgv2") ? $errItem->Msgv2 : $errItem->MSGV2;
-                $Msgv3 = property_exists($errItem, "Msgv3") ? $errItem->Msgv3 : $errItem->MSGV3;
-                $Msgv4 = property_exists($errItem, "Msgv4") ? $errItem->Msgv4 : $errItem->MSGV4;
-                $Msgid = property_exists($errItem, "Msgid") ? $errItem->Msgid : $errItem->MSGID;
-                
-                // abort or error should be thrown
-                if ( $Msgty == ErrorMessage::ERR_TYPE_ERROR || $Msgty == ErrorMessage::ERR_TYPE_ABORT ){
-                    // if batch is not unique
-                    if ( $Msgno == Constants::BATCH_NUMBER_NOT_UNIQUE ){
-                        throw new BatchNumberNotUniqueException( $Msgv1 );
-                    }
-                    
-                    throw new SoapException( $Message, $Msgno, $Msgv1, $Msgv2, $Msgv3, $Msgv4, $Msgid );			
-                }
-				
-        		// info or warning should be set in the response object for later
-                $errorMessages[] = new ErrorMessage( $Msgty, $Msgno, $Msgid, $Msgv1, $Msgv2, $Msgv3, $Msgv4, $Message );
-            }
-            $response->setErrorMessages($errorMessages);
-        }
-        return $response;
+	    if (isset($rawResponse->ErrorList->item)){
+	        $errItems = is_array($rawResponse->ErrorList->item) ? $rawResponse->ErrorList->item : [$rawResponse->ErrorList->item];
+	        if (count($errItems) > 0)
+	        {
+	            $errorMessages = [];
+	            foreach ($errItems as $errItem)
+	            {
+	                $Msgty = property_exists($errItem, "Msgty") ? $errItem->Msgty : $errItem->MSGTY;
+	                $Message = property_exists($errItem, "Message") ? $errItem->Message : $errItem->MESSAGE;
+	                $Msgno = property_exists($errItem, "Msgno") ? $errItem->Msgno : $errItem->MSGNO;
+	                $Msgv1 = property_exists($errItem, "Msgv1") ? $errItem->Msgv1 : $errItem->MSGV1;
+	                $Msgv2 = property_exists($errItem, "Msgv2") ? $errItem->Msgv2 : $errItem->MSGV2;
+	                $Msgv3 = property_exists($errItem, "Msgv3") ? $errItem->Msgv3 : $errItem->MSGV3;
+	                $Msgv4 = property_exists($errItem, "Msgv4") ? $errItem->Msgv4 : $errItem->MSGV4;
+	                $Msgid = property_exists($errItem, "Msgid") ? $errItem->Msgid : $errItem->MSGID;
+	                
+	                // abort or error should be thrown
+	                if ( $Msgty == ErrorMessage::ERR_TYPE_ERROR || $Msgty == ErrorMessage::ERR_TYPE_ABORT ){
+	                    // if batch is not unique
+	                    if ( $Msgno == Constants::BATCH_NUMBER_NOT_UNIQUE ){
+	                        throw new BatchNumberNotUniqueException( $Msgv1 );
+	                    }
+	                    
+	                    throw new SoapException( $Message, $Msgno, $Msgv1, $Msgv2, $Msgv3, $Msgv4, $Msgid );
+	                }
+	                
+	                // info or warning should be set in the response object for later
+	                $errorMessages[] = new ErrorMessage( $Msgty, $Msgno, $Msgid, $Msgv1, $Msgv2, $Msgv3, $Msgv4, $Message );
+	            }
+	            $response->setErrorMessages($errorMessages);
+	        }
+	    }
+	    
+	    return $response;
 	}
 }
